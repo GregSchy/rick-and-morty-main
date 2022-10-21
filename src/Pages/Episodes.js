c071de70816346e5e0d4fdcc28c0595b8eebdf77
GregSchy <105386198+GregSchy@users.noreply.github.com>
2022-10-21 18:07:59 +0200

    Update Episodes.js

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Episode() {
+  let [id, setID] = useState(1)
+  let api = `https://rickandmortyapi.com/api/episode/1`
+
+  useEffect(()=>{
+    (async function(){
+      let data = await fetch(api).then(res=>res.json())
+      console.log(data)
+    })()
+  },[api])
+
   return (
     <div>The episodes are there.</div>
   )
