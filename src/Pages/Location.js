2cee38e56126c81ed64db5161abe0bf12ece36c8
GregSchy <105386198+GregSchy@users.noreply.github.com>
2022-10-22 15:03:11 +0200

    Added Card Details
    04:04:04

--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Cards } from '../components/Cards/Cards'
+import InputGroup from '../components/Filters/Category/InputGroup'
+
+
+
+
 
 function Location() {
+  let [id, setID] = useState(1)
+  let [info, setInfo] = useState([])
+  let [results, setResults] = useState([])
+  let { name, type, dimension } = info
+  let api = `https://rickandmortyapi.com/api/location/${id}`
+
+  useEffect(()=>{
+    (async function(){
+      let data = await fetch(api).then(res=>res.json())
+      setInfo(data);
+
+      let a = await Promise.all(
+        data.residents.map((x)=>{
+          return fetch(x).then(res=>res.json())
+        })
+      );
+      setResults(a)
+    })()
+  },[api])
+
   return (
-    <div>The locations are here.</div>
+    <div className="container">
+      <div className="row mb-4">
+        <h1 className="text-center mb-4">
+        Location : {" "}
+        <span className="text-primary">
+         {name===""? "Unknown" : name}
+        </span>
+         </h1>
+        <h5 className="text-center">
+          Dimension: {" "}
+          <span className="text-primary">
+           {dimension ===""? "Unknown" : dimension}
+          </span> 
+        </h5>
+      </div>
+      <div className="row"> 
+        <div className="col-3">
+        <h4 className="text-center mb-4">Pick location</h4>
+        <InputGroup setID={setID} name="Location" total={126} />
+        </div>
+        <div className="col-8">
+         <div className="row">
+            <Cards results={results}  />
+         </div> 
+        </div>
+      </div>
+    </div>
   )
 }
 
