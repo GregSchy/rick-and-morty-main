a15c17c5f7debe10d11e2ee0b98c8d0a4810c93c
GregSchy <105386198+GregSchy@users.noreply.github.com>
2022-10-24 12:38:54 +0200

    Responsive version
    No pagination

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const Home= () => {
           
             <Filters setSpecies={setSpecies} setStatus={setStatus} setPageNumber={setPageNumber} setGender={setGender} />
           
-          <div className="col-8">
+          <div className="col-lg-8 col-12">
             <div className="row">
               <Cards page="/" results={results} />
             </div>
--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -44,11 +44,11 @@ function Location() {
         </h5>
       </div>
       <div className="row"> 
-        <div className="col-3">
+        <div className="col-lg-3 col-12">
         <h4 className="text-center mb-4">Pick location</h4>
         <InputGroup setID={setID} name="Location" total={126} />
         </div>
-        <div className="col-8">
+        <div className="col-lg-8 col-12">
          <div className="row">
             <Cards page="/locations/" results={results}  />
          </div> 
